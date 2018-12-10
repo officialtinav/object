0c84a16c8b27122180d6963a82ef4e876dbf4dd1
tina <34047417+officialtinav@users.noreply.github.com>
2018-12-10 12:34:08 -1000

    Add files via upload

--- scratchoff.js
+++ scratchoff.js
@@ -9,9 +9,9 @@ step 1: build div box that will fade when the mouse is dragged over it
 
 var op = 1;
 function scratchMe() {
-  document.getElementById('scratchCover')
+  
   op -=0.1;
   //test to see if the op is decreasing
   document.getElementById('test').innerHTML = op;
-  document.style.opacity = op;
+  document.getElementById('scratchMe').style.opacity = op;
 }
